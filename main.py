7f0eedbc0d2a6625198e77444ee387d5803b887e
raderth <50417647+raderth@users.noreply.github.com>
2024-08-13 14:18:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import subprocess
+
+async def run_file(file):
+    process = await asyncio.create_subprocess_exec(
+        'python', file,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    if stdout:
+        print(f'{file} output:\n{stdout.decode()}')
+    if stderr:
+        print(f'{file} error:\n{stderr.decode()}')
+
+async def main():
+    await asyncio.gather(
+        run_file('web.py'),
+        #run_file('bot.py')
+    )
+
+asyncio.run(main())
